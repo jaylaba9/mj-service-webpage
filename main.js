12e67509bc594c942c3c86f2176617e54467119a
Jaroslaw Laba <jarek.laba@gmail.com>
2023-02-20 22:30:26 +0100

    finishing layout

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const primaryNav = document.querySelector('.primary-navigation');
 const toggleNav = document.querySelector('.mobile-nav-toggle');
 const toggleNavIcon = document.querySelector('.mobile-nav-toggle i');
 
+const accBtn = document.querySelectorAll('.accordion-btn');
+
+/* ------- NAVIGATION ------- */
 toggleNav.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible');
 
@@ -21,3 +24,18 @@ toggleNav.addEventListener('click', () => {
     toggleNavIcon.classList.add('fa-bars');
   }
 });
+
+/* ------- ACCORDION ------- */
+accBtn.forEach((btn) =>
+  btn.addEventListener('click', function () {
+    this.classList.toggle('active');
+    // toggle between hiding and showing the active panel
+    const panel = this.nextElementSibling;
+    console.log(panel);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  })
+);
