dc256a756ad176f8bcbaba4bfd9fa5bf178cfd37
Jaroslaw Laba <jaroslaw.laba@comarch.com>
2023-02-23 15:45:15 +0100

    some changes

--- main.js
+++ main.js
@@ -1,41 +1,78 @@
-'use strict';
+"use strict";
 
-const primaryNav = document.querySelector('.primary-navigation');
-const toggleNav = document.querySelector('.mobile-nav-toggle');
-const toggleNavIcon = document.querySelector('.mobile-nav-toggle i');
+const primaryNav = document.querySelector(".primary-navigation");
+const toggleNav = document.querySelector(".mobile-nav-toggle");
+const toggleNavIcon = document.querySelector(".mobile-nav-toggle i");
 
-const accBtn = document.querySelectorAll('.accordion-btn');
+const accBtn = document.querySelectorAll(".accordion-btn");
 
-/* ------- NAVIGATION ------- */
-toggleNav.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible');
+const scrollAbout = document.querySelectorAll(".scroll-into-about");
+const scrollOffering = document.querySelector(".scroll-into-offering");
+const scrollJobs = document.querySelector(".scroll-into-jobs");
+const scrollProjects = document.querySelector(".scroll-into-projects");
+const scrollContact = document.querySelectorAll(".scroll-into-contact");
 
-  if (visibility === 'false') {
-    primaryNav.setAttribute('data-visible', true);
-    toggleNav.setAttribute('aria-expanded', true);
+const sectionAbout = document.querySelector(".about");
+const sectionOffering = document.querySelector(".offering");
+const sectionJobs = document.querySelector(".jobs");
+const sectionProjects = document.querySelector(".projects");
+const sectionContact = document.querySelector(".contact");
 
-    toggleNavIcon.classList.remove('fa-bars');
-    toggleNavIcon.classList.add('fa-xmark');
+/* ------- NAVIGATION BAR ------- */
+toggleNav.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    toggleNav.setAttribute("aria-expanded", true);
+
+    toggleNavIcon.classList.remove("fa-bars");
+    toggleNavIcon.classList.add("fa-xmark");
   } else {
-    primaryNav.setAttribute('data-visible', false);
-    toggleNav.setAttribute('aria-expanded', false);
+    primaryNav.setAttribute("data-visible", false);
+    toggleNav.setAttribute("aria-expanded", false);
 
-    toggleNavIcon.classList.remove('fa-xmark');
-    toggleNavIcon.classList.add('fa-bars');
+    toggleNavIcon.classList.remove("fa-xmark");
+    toggleNavIcon.classList.add("fa-bars");
   }
 });
 
 /* ------- ACCORDION ------- */
 accBtn.forEach((btn) =>
-  btn.addEventListener('click', function () {
-    this.classList.toggle('active');
+  btn.addEventListener("click", function () {
+    this.classList.toggle("active");
     // toggle between hiding and showing the active panel
     const panel = this.nextElementSibling;
     console.log(panel);
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
+  })
+);
+
+/* ------- SCROLLING ------- */
+scrollAbout.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    sectionAbout.scrollIntoView();
+  })
+);
+
+scrollOffering.addEventListener("click", () => {
+  sectionOffering.scrollIntoView();
+});
+
+scrollJobs.addEventListener("click", () => {
+  sectionJobs.scrollIntoView();
+});
+
+scrollProjects.addEventListener("click", () => {
+  sectionProjects.scrollIntoView();
+});
+
+scrollContact.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    sectionContact.scrollIntoView();
   })
 );
